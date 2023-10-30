a88c3d364f65533f8f5c1ef634b784aca84f0ef5
malamadre211 <mariangelacurcioartist@gmail.com>
2023-10-30 12:15:55 +0100

    exe 4 ok

---
+++ tp_react/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import NameForm from './main.tsx';
+import HelloWorld from './main.tsx';
+import Counter from './main.tsx';
+import List from './main.tsx';
+
+
+const App = () => {
+  return (
+      <div>
+          <NameForm />
+      </div>
+  );
+};
+
+export default App;
